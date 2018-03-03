082e7a36a20ced198e607f323d794c5beb912354
Vikas Sharma <vs8908@gmail.com>
2018-03-03 12:00:19 +0530

    Admin create user,post,publish and unpublish also

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -5,9 +5,19 @@ ActiveAdmin.register Post do
 		link_to "Publish", publish_admin_post_path(post), method: :put if !post.published_at?
 	end
 
+	action_item :publish, only: :show do
+		link_to "Unpublish", unpublish_admin_post_path(post), method: :put if post.published_at?
+	end
+
 	member_action :publish, method: :put do
 		post = Post.find(params[:id])
 		post.update(published_at: Time.zone.now)
+		redirect_to admin_post_path(post)
+	end
+
+	member_action :unpublish, method: :put do
+		post = Post.find(params[:id])
+		post.update(published_at: nil)
 		redirect_to admin_post_path(post)
 	end
 end
